0ca7a4868079fc1b9e042398b5ebd9550d122944
yanCode <yan.code@gmail.com>
2013-07-11 20:07:39 -0700

    add login & addAuthKey functions.

--- models/userModel.js
+++ models/userModel.js
@@ -5,6 +5,7 @@
  */
 
 var mongoose = require('mongoose')
+    , uuid = require('node-uuid')
     , Schema = mongoose.Schema
     , ObjectId = Schema.ObjectId
     , userSchema
@@ -54,6 +55,44 @@ userSchema.methods = {
 };
 
 /**
+ * User static method.
+ * @type {{login: Function}}
+ */
+userSchema.statics = {
+
+    /**
+     * Login function, upon succeeded, the id logged user will be returned for future process.
+     *
+     * @param user {Object}, containing username & password.
+     * @param done {Function(err,userId,message)}, callback function, if login succeeded, userId will be returned,
+     *        otherwise userId will be none,with a message provided as the error message. Any system err, will be
+     *        passed in first parameter err.
+     * @returns {*}
+     */
+    login: function (user, done) {
+        if (!(user && user.username && user.password)) {
+            return done(null, null, 'username & password are required');
+        }
+        this.findOne({username: user.username}, function (err, doc) {
+            //system error.
+            if (err) {
+                return   done(err);
+            }
+            //user doesn't exit.
+            if (!doc) {
+                return  done(null, null, "user doesn't exit!");
+            }
+            //password correct.
+            if (doc.comparePassword(user.password)) {
+                done(null, doc.id);
+            } else {
+                done(null, null, "password incorrect!");
+            }
+        });
+    }
+};
+
+/**
  * authSchema.
  *
  * @type {Schema}
@@ -74,6 +113,10 @@ authSchema = new Schema({
     }
 });
 authSchema.methods = {
+    /**
+     * each time when a logged user accesses any api, the effective date of this authKey should be update to renew the
+     * effective date to accessing time.
+     */
     updateEffectiveDate: function () {
         var auth = this,
             now = new Date();
@@ -84,6 +127,26 @@ authSchema.methods = {
         }
     }
 };
+
+authSchema.statics = {
+
+    /**
+     * add a authKey to database, this authkey is a time-based UUID.
+     * @param userId
+     * @param done
+     */
+    addAuthKey: function (userId, done) {
+        var Auth = this,
+        //using time-based uuid as authKey.
+            authKey = uuid.v1(),
+            auth = new Auth({user: userId, authKey: authKey});
+        auth.save(function (err) {
+            done(err, authKey);
+        });
+    }
+};
+
+
 //create and set two models into mongoose instance, they can be fetched anywhere mongoose object is presented.
 mongoose.model('User', userSchema);
 mongoose.model('Auth', authSchema);
