4358a9df141adb019f098ee0f4c3f2d30f88a3b1
yanCode <yan.code@gmail.com>
2013-07-11 22:30:36 -0700

    show the info of logged user

--- controllers/demoController.js
+++ controllers/demoController.js
@@ -3,7 +3,7 @@
 var demoController = function (app) {
 
     app.get('/api/time', function (req, res) {
-        res.json(200, {'message': 'This is time page API!'});
+        res.json(200, {'message': 'This is time page API!', 'user': req.user});
     });
 
     app.get('/api/admin/time', function (req, res) {
